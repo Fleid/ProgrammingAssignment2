4502bf17816ed5f0f1da60b43d4eb2224516fc3d
Florian <florian.eiden@fleid.fr>
2014-09-18 16:04:27 +0200

    Implementation Solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Expose les 4 fonctions qui gèrent la mise en cache (get,set,setmean,getmean)
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## Initialisation
+  m <- NULL
+  ## Fonctions : on manipule des paires clefs/valeurs avec le résultat, stockées dans l'env de la fonction
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  
+  ## On retourne la liste des fonctions
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
+## Gère effectivement la mise en cache à partir des 4 fonctions mises à dispo
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## On va chercher la valeur déjà stockée
+  ## Si elle existe déjà, on a déjà fait le calcul: on retourne la valeur stockée
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## Sinon on va chercher la valeur...
+  data <- x$get()
+  ## ...et avec solve on calcule l'inverse de la matrice
+  m <- solve(data, ...)
+  ## On l'enregistre et on la retourne
+  x$setmean(m)
+  m
 }
+
+
